f871c3a275114341550a59334a57a4e21df296d9
Hosick <ghtjr5000@naver.com>
2019-11-22 19:24:52 +0900

    20191122

---
+++ chap3/ex3_7.js
@@ -0,0 +1,17 @@
+var foo = {
+    name : 'foo',
+    major : 'computer science'
+};
+
+console.log(foo.name);
+console.log(foo['name']);
+console.log(foo.nickname);
+
+foo.age = 30;
+console.log(foo.age);
+
+foo['full-name'] = 'foo bar';
+console.log(foo['full-name']);
+console.log(foo.full-name);
+console.log(foo.full);
+console.log(name);
