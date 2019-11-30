378beb296c6c1198cbda7012d5102d3ed6422841
Hosick <ghtjr5000@naver.com>
2019-11-30 18:37:29 +0900

    20191130

---
+++ chap6/ex6_7.js
@@ -0,0 +1,22 @@
+function Person(arg) {
+    this.name = arg;
+}
+
+Person.prototype.setName = function (value) {
+    this.name = value;
+}
+
+Person.prototype.getName = function () {
+    return this.name;
+}
+
+function Student() {
+    Person.apply(this, arguments);
+}
+
+var you = new Person("iamhjoo");
+Student.prototype = you;
+
+var me = new Student("zzoon");
+me.setName("zzoon");
+console.log(me.getName());
